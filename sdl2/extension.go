4286fa3bb89c682b0358f8497ffb730945810a8a
Stephen Baynham <sdbaynham@gmail.com>
2022-07-29 22:23:49 -0700

    Forgot to add godoc

--- sdl2/extension.go
+++ sdl2/extension.go
@@ -14,6 +14,13 @@ import (
 	"unsafe"
 )
 
+// CreateSurface generates a khr_surface.Surface from an SDL2 Window
+//
+// instance - The Instance that will own the new Surface
+//
+// extension - A khr_surface.Extension object
+//
+// window - And SDL2 Window that the new khr_surface.Surface will represent
 func CreateSurface(instance core1_0.Instance, extension khr_surface.Extension, window *sdl.Window) (khr_surface.Surface, error) {
 	surfacePtrUnsafe, err := window.VulkanCreateSurface((*C.VkInstance)(unsafe.Pointer(instance.Handle())))
 	if err != nil {
